bef346105263651fc1866980fd170a9abfd911ae
Kevin Garcia <ermianrazor@gmail.com>
2025-05-26 19:12:38 -0500

    Agregar archivo index.py con la estructura de la página de inicio y componentes necesarios

---
+++ peluqueria/pages/index.py
@@ -0,0 +1,35 @@
+import reflex as rx
+
+from peluqueria.components.navbar import navbar
+from peluqueria.views.about.about import about
+from peluqueria.views.hero.hero import hero
+from peluqueria.views.services.services import services
+from peluqueria.views.contact.contact import contact
+from peluqueria.components.footer import footer
+from peluqueria.utils import lang
+
+@rx.page(
+        route="/",
+        title="Inicio | Peluquería",
+        description="Reserva tu cita en nuestra peluquería y luce el estilo que mereces. Cortes, color y más, con atención personalizada.",
+        meta= [
+            {"char_set": "UTF-8"},
+        ]
+)
+def index() -> rx.Component:
+    return rx.vstack(
+        lang(),
+        navbar(),
+        hero(),
+        rx.vstack(
+            services(),
+            about(),
+            contact(),
+            max_width="85%",
+            margin_top="2rem",
+            spacing="8",
+        ),
+        footer(),
+        spacing="0",
+        align="center"
+    )
