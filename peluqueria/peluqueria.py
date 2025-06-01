72aa496489f409a2a0742220fc9ed0381d36ac1a
Kevin Garcia <ermianrazor@gmail.com>
2025-06-01 18:21:33 -0500

    Agregar el transformador de API a la configuración de la aplicación en peluqueria.py

--- peluqueria/peluqueria.py
+++ peluqueria/peluqueria.py
@@ -3,11 +3,13 @@ import reflex as rx
 from peluqueria.styles.styles import BASE
 from peluqueria.pages.index import index
 from peluqueria.pages.register import register
+from peluqueria.api.main import fastapi_app
 
 app: rx.App = rx.App(
     theme=rx.theme(
         appearance="light",
     ),
     style=BASE, # type: ignore
-    stylesheets=["/css/normalize.css"]
+    stylesheets=["/css/normalize.css"],
+    api_transformer=fastapi_app,
 )
