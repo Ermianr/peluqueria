69d3b392c13d5b9363918e6e8a41b2f6aca11104
Kevin Garcia <ermianrazor@gmail.com>
2025-06-09 23:11:22 -0500

    Reorganizar importaciones en el archivo principal de la aplicación

--- peluqueria/peluqueria.py
+++ peluqueria/peluqueria.py
@@ -1,15 +1,16 @@
 import reflex as rx
 
-from peluqueria.styles.styles import BASE
-from peluqueria.pages.index import index
-from peluqueria.pages.register import register
 from peluqueria.api.main import fastapi_app
+from peluqueria.pages.index import index
+from peluqueria.pages.login import login
+from peluqueria.pages.register import register
+from peluqueria.styles.styles import BASE
 
 app: rx.App = rx.App(
     theme=rx.theme(
         appearance="light",
     ),
-    style=BASE, # type: ignore
+    style=BASE,  # type: ignore
     stylesheets=["/css/normalize.css"],
     api_transformer=fastapi_app,
 )
