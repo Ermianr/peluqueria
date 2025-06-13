9eb916a2692e639a5fc15f823bbf93c2ab2fa1bc
Kevin Garcia <ermianrazor@gmail.com>
2025-06-13 13:10:11 -0500

    Eliminar líneas en blanco y reorganizar importaciones en main.py

--- peluqueria/api/main.py
+++ peluqueria/api/main.py
@@ -1,9 +1,8 @@
 from fastapi import FastAPI
-from peluqueria.api.routers import users
+
+from peluqueria.api.routers import auth, users
 
 fastapi_app: FastAPI = FastAPI()
 
 fastapi_app.include_router(users.router)
-
-
-
+fastapi_app.include_router(auth.router)
