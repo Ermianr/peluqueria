c7e6bc0f38f84be3d6c96ff7623d5b0aae619ced
Kevin Garcia <ermianrazor@gmail.com>
2025-06-13 13:10:53 -0500

    Renombrar esquemas de respuesta de usuario y agregar campos de creación y contraseña hasheada para mejorar la estructura de datos.

--- peluqueria/api/schemas/user.py
+++ peluqueria/api/schemas/user.py
@@ -1,14 +1,25 @@
-def creation_user_schema(user) -> dict:
+def user_response_schema(user) -> dict:
     return {
-        "id": str(user.get("_id", "")),
-    }
-
-
-def find_user_schema(user) -> dict:
-    return {
-        "id": str(user.get("_id", "")),
         "first_name": user.get("first_name", ""),
         "last_name": user.get("last_name", ""),
         "email": user.get("email", ""),
         "phone": user.get("phone", ""),
+        "id": str(user.get("_id", "")),
+        "created_at": user.get("created_at", ""),
+        "updated_at": user.get("updated_at", ""),
+        "is_active": user.get("is_active", ""),
+    }
+
+
+def user_db_response_schema(user) -> dict:
+    return {
+        "first_name": user.get("first_name", ""),
+        "last_name": user.get("last_name", ""),
+        "email": user.get("email", ""),
+        "phone": user.get("phone", ""),
+        "id": str(user.get("_id", "")),
+        "hashed_password": user.get("hashed_password", ""),
+        "created_at": user.get("created_at", ""),
+        "updated_at": user.get("updated_at", ""),
+        "is_active": user.get("is_active", ""),
     }
