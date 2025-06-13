e87df10c2c23b60d5ca58551b3fed2e14e75376a
Kevin Garcia <ermianrazor@gmail.com>
2025-06-13 13:10:40 -0500

    Actualizar la validación del número de teléfono utilizando PHONE_REGEX y reorganizar las clases de respuesta de usuario

--- peluqueria/api/models/user.py
+++ peluqueria/api/models/user.py
@@ -3,8 +3,10 @@ from typing import Annotated
 
 from pydantic import BaseModel, EmailStr, StringConstraints
 
+from peluqueria.constants import PHONE_REGEX
+
 Name = Annotated[str, StringConstraints(strip_whitespace=True, min_length=4)]
-Phone = Annotated[str, StringConstraints(pattern=r"^3\d{9}$")]
+Phone = Annotated[str, StringConstraints(pattern=PHONE_REGEX)]
 
 
 class User(BaseModel):
@@ -12,18 +14,22 @@ class User(BaseModel):
     last_name: Name
     email: EmailStr
     phone: Phone
+
+
+class UserCreate(User):
     password: str
+
+
+class UserInDB(User):
+    id: str | None = None
+    hashed_password: str
     created_at: datetime | None = None
     updated_at: datetime | None = None
+    is_active: bool = True
 
 
-class UserCreationResponse(BaseModel):
+class UserResponse(User):
     id: str
-
-
-class UserFindResponse(BaseModel):
-    id: str
-    first_name: str
-    last_name: str
-    email: str
-    phone: str
+    created_at: datetime
+    updated_at: datetime
+    is_active: bool
