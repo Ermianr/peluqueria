37101acdedc9273927158b32f8522e615a9676e7
Kevin Garcia <ermianrazor@gmail.com>
2025-06-18 10:29:02 -0500

    Refactor esquemas de respuesta de usuario: mejora la obtención de datos utilizando acceso directo a claves y elimina valores predeterminados innecesarios.

--- peluqueria/api/schemas/user.py
+++ peluqueria/api/schemas/user.py
@@ -1,25 +1,31 @@
 def user_response_schema(user) -> dict:
     return {
-        "first_name": user.get("first_name", ""),
-        "last_name": user.get("last_name", ""),
-        "email": user.get("email", ""),
-        "phone": user.get("phone", ""),
-        "id": str(user.get("_id", "")),
-        "created_at": user.get("created_at", ""),
-        "updated_at": user.get("updated_at", ""),
-        "is_active": user.get("is_active", ""),
+        "first_name": user["first_name"],
+        "last_name": user["last_name"],
+        "email": user["email"],
+        "phone": user["phone"],
+        "id": str(user["_id"]),
+        "role": user["role"],
+        "created_at": user["created_at"],
+        "updated_at": user["updated_at"],
+        "is_active": user["is_active"],
     }
+
+
+def users_response_schema(users) -> list:
+    return [user_response_schema(user) for user in users]
 
 
 def user_db_response_schema(user) -> dict:
     return {
-        "first_name": user.get("first_name", ""),
-        "last_name": user.get("last_name", ""),
-        "email": user.get("email", ""),
-        "phone": user.get("phone", ""),
+        "first_name": user["first_name"],
+        "last_name": user["last_name"],
+        "email": user["email"],
+        "phone": user["phone"],
         "id": str(user.get("_id", "")),
-        "hashed_password": user.get("hashed_password", ""),
-        "created_at": user.get("created_at", ""),
-        "updated_at": user.get("updated_at", ""),
-        "is_active": user.get("is_active", ""),
+        "role": user["role"],
+        "hashed_password": user["hashed_password"],
+        "created_at": user["created_at"],
+        "updated_at": user["updated_at"],
+        "is_active": user["is_active"],
     }
