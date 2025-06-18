8a6e4cff978e4c79fb6952b7f4e944e1be9e29a7
Kevin Garcia <ermianrazor@gmail.com>
2025-06-18 10:26:53 -0500

    Agregar importaciones de appointments y users_manage en peluqueria.py para mejorar la estructura del proyecto.

--- peluqueria/peluqueria.py
+++ peluqueria/peluqueria.py
@@ -1,6 +1,8 @@
 import reflex as rx
 
 from peluqueria.api.main import fastapi_app
+from peluqueria.pages.appointments import appointments
+from peluqueria.pages.dashboard import users_manage
 from peluqueria.pages.index import index
 from peluqueria.pages.login import login
 from peluqueria.pages.register import register
