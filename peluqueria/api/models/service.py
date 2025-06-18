d0d0e17624dcc3e911307d691dc35b81baf5bf3e
Kevin Garcia <ermianrazor@gmail.com>
2025-06-18 10:28:12 -0500

    Agregar modelo Service y clases relacionadas para gestionar servicios en la API.

---
+++ peluqueria/api/models/service.py
@@ -0,0 +1,30 @@
+from datetime import datetime
+
+from pydantic import BaseModel
+
+from peluqueria.api.models.types import DateCo
+
+
+class Service(BaseModel):
+    name: str
+    description: str | None = None
+    duration_minutes: int
+    price: int
+
+
+class ServiceInDB(Service):
+    created_at: datetime
+    updated_at: datetime
+
+
+class ServiceResponse(Service):
+    id: str
+    created_at: DateCo
+    updated_at: DateCo
+
+
+class ServiceUpdateAdmin(BaseModel):
+    name: str | None = None
+    description: str | None = None
+    duration_minutes: int | None = None
+    price: int | None = None
