d01c6f18e5a1accfa55ed060dbe8bde479e5477c
Kevin Garcia <ermianrazor@gmail.com>
2025-06-17 20:36:07 -0500

    Refactor el formulario de registro para manejar la validación de campos y la carga de datos de manera asíncrona, mejorando la experiencia del usuario y la gestión de errores.

--- peluqueria/views/register/register_form/register_form.py
+++ peluqueria/views/register/register_form/register_form.py
@@ -6,102 +6,103 @@ import reflex as rx
 from peluqueria.components.form_field_password import form_field_password
 from peluqueria.components.form_field_state import form_field_state
 from peluqueria.constants import EMAIL_REGEX, PHONE_REGEX
+from peluqueria.settings import Settings
 from peluqueria.styles.styles import SOLID_BUTTON, Colors
 
 
 class RegisterState(rx.State):
-    form_data: dict = {}
+    loading: bool = False
 
     initial_first_name_error_state: str = "none"
-    first_name_value = ""
 
     initial_last_name_error_state: str = "none"
-    last_name_value = ""
 
     initial_email_error_state: str = "none"
-    email_value: str = ""
 
     initial_phone_error_state: str = "none"
-    phone_value: str = ""
 
     initial_password_error_state: str = "none"
-    password_value = ""
-
-    @rx.event
-    def first_name_validation(self) -> None:
-        if len(self.first_name_value) > 2:
-            self.initial_first_name_error_state: str = "none"
-        else:
-            self.initial_first_name_error_state: str = "block"
-
-    @rx.event
-    def last_name_validation(self) -> None:
-        if len(self.last_name_value) > 2:
-            self.initial_last_name_error_state: str = "none"
-        else:
-            self.initial_last_name_error_state: str = "block"
-
-    @rx.event
-    def email_validation(self) -> None:
-        if len(self.email_value) == 0:
-            self.initial_email_error_state = "block"
-        elif re.match(EMAIL_REGEX, self.email_value):
-            self.initial_email_error_state = "none"
-        else:
-            self.initial_email_error_state = "block"
-
-    @rx.event
-    def phone_validation(self) -> None:
-        if len(self.phone_value) == 0:
-            self.initial_phone_error_state = "block"
-        elif re.match(PHONE_REGEX, self.phone_value):
-            self.initial_phone_error_state = "none"
-        else:
-            self.initial_phone_error_state = "block"
-
-    @rx.event
-    def password_validation(self, value: str) -> None:
-        self.password_value = value
-        if len(self.password_value) >= 8:
-            self.initial_password_error_state: str = "none"
-        else:
-            self.initial_password_error_state: str = "block"
 
     @rx.event
     async def handle_submit(self, form_data: dict):
-        self.first_name_value = ""
-        self.last_name_value = ""
-        self.email_value = ""
-        self.phone_value = ""
-        self.password_value = ""
-        print(self.form_data)
+        self.loading = True
+        valid_first_name: bool = False
+        valid_last_name: bool = False
+        valid_email: bool = False
+        valid_phone: bool = False
+        valid_password: bool = False
+
+        if len(form_data.get("first_name") or "") >= 3:
+            valid_first_name = True
+            self.initial_first_name_error_state = "none"
+        else:
+            valid_first_name = False
+            self.initial_first_name_error_state = "block"
+
+        if len(form_data.get("last_name") or "") >= 3:
+            valid_last_name = True
+            self.initial_last_name_error_state = "none"
+        else:
+            valid_last_name = False
+            self.initial_last_name_error_state = "block"
+
+        if re.match(EMAIL_REGEX, form_data.get("email") or ""):
+            valid_email = True
+            self.initial_email_error_state = "none"
+        else:
+            valid_email = False
+            self.initial_email_error_state = "block"
+
+        if re.match(PHONE_REGEX, form_data.get("phone") or ""):
+            valid_phone = True
+            self.initial_phone_error_state = "none"
+        else:
+            valid_phone = False
+            self.initial_phone_error_state = "block"
+
+        if len(form_data.get("password") or "") >= 8:
+            valid_password = True
+            self.initial_password_error_state = "none"
+        else:
+            valid_password = False
+            self.initial_password_error_state = "block"
+
         if (
-            re.match(EMAIL_REGEX, form_data.get("phone") or "")
-            and re.match(PHONE_REGEX, form_data.get("email") or "")
-            and len(form_data.get("first_name") or "") >= 3
-            and len(form_data.get("last_name") or "") >= 3
-            and len(form_data.get("password") or "") >= 8
+            valid_first_name
+            and valid_last_name
+            and valid_email
+            and valid_phone
+            and valid_password
         ):
-            self.form_data = form_data
+            form_data["first_name"] = form_data["first_name"].capitalize()
+            form_data["last_name"] = form_data["last_name"].capitalize()
             try:
-                async with httpx.AsyncClient() as client:
+                async with httpx.AsyncClient(
+                    base_url=Settings.API_BACKEND_URL
+                ) as client:
                     response = await client.post(
-                        "http://localhost:8000/users/",
-                        json=self.form_data,
+                        "/users",
+                        json=form_data,
                         timeout=10.0,
                     )
 
                 if response.status_code == 201:
-                    print("Usuario creado correctamente")
                     yield rx.toast.success("Usuario creado exitosamente.")
-                else:
-                    print(f"Error: {response.status_code}")
+                    yield rx.redirect("/ingreso")
+                    self.loading = False
+                elif response.status_code == 409:
+                    yield rx.toast.error("Error: Este usuario ya existe.")
+                    self.loading = False
             except httpx.RequestError as e:
-                print(f"Error de conexión: {e}")
+                print(f"Error: Error de conexión: {e}")
+                self.loading = False
+            except Exception:
+                yield rx.toast.error("Error: Error inesperado")
         else:
             yield rx.toast.error(
-                "Por favor verifique los datos e inténtalo nuevamente."
+                "Error: Por favor verifique los datos e inténtalo nuevamente."
             )
+            self.loading = False
 
 
 def register_form() -> rx.Component:
@@ -123,9 +124,6 @@ def register_form() -> rx.Component:
                         "text",
                         "first_name",
                         RegisterState.initial_first_name_error_state,
-                        RegisterState.first_name_value,
-                        RegisterState.set_first_name_value,  # type: ignore
-                        RegisterState.first_name_validation,
                         "Mínimo 3 caracteres.",
                     ),
                     form_field_state(
@@ -134,9 +132,6 @@ def register_form() -> rx.Component:
                         "text",
                         "last_name",
                         RegisterState.initial_last_name_error_state,
-                        RegisterState.last_name_value,
-                        RegisterState.set_last_name_value,  # type: ignore
-                        RegisterState.last_name_validation,
                         "Mínimo 3 caracteres.",
                     ),
                     direction=rx.breakpoints(
@@ -153,9 +148,6 @@ def register_form() -> rx.Component:
                         "email",
                         "email",
                         RegisterState.initial_email_error_state,
-                        RegisterState.email_value,
-                        RegisterState.set_email_value,  # type: ignore
-                        RegisterState.email_validation,
                         "Por favor, ingresa un correo electrónico valido.",
                     ),
                     margin_bottom="0.5rem",
@@ -167,38 +159,52 @@ def register_form() -> rx.Component:
                         "tel",
                         "phone",
                         RegisterState.initial_phone_error_state,
-                        RegisterState.phone_value,
-                        RegisterState.set_phone_value,  # type: ignore
-                        RegisterState.phone_validation,
                         "Por favor, ingresa un número de teléfono Colombiano valido.",
                     ),
                     margin_bottom="0.5rem",
                 ),
                 rx.box(
                     form_field_password(
-                        "Contraseña",
+                        rx.text(
+                            "Contraseña ",
+                            rx.text(
+                                "(Mínimo 8 caracteres)",
+                                color=Colors.PARAGRAPH_COLOR.value,
+                                as_="span",
+                            ),
+                        ),
                         "Contraseña",
                         "password",
                         "password",
                         RegisterState.initial_password_error_state,
-                        RegisterState.password_value,
-                        RegisterState.password_validation,
                         "Por favor ingrese una contraseña de mínimo 8 caracteres.",
                     ),
                     margin_bottom="1rem",
                 ),
                 rx.separator(margin_bottom="1rem"),
+                rx.text(
+                    "¿Ya tienes una cuenta? ",
+                    rx.link(
+                        "Ingresa desde aquí",
+                        href="/ingreso",
+                        color=Colors.PRIMARY_COLOR.value,
+                    ),
+                    margin_bottom="1rem",
+                    size="2",
+                ),
                 rx.button(
-                    "Registrarse",
+                    rx.cond(RegisterState.loading, rx.spinner(size="2"), "Registrarse"),
                     type="submit",
                     style=SOLID_BUTTON,
                     padding_y="1.2rem",
+                    disabled=RegisterState.loading,
                 ),
                 on_submit=RegisterState.handle_submit,
+                reset_on_submit=True,
             ),
             width=rx.breakpoints(initial="80%", md="40%"),
         ),
         margin_y="2rem",
-        height="60vh",
+        min_height="60vh",
         width="100%",
     )
