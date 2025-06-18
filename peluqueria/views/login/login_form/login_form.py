206a02830c0eb3ae5023c05528072664a155f9b7
Kevin Garcia <ermianrazor@gmail.com>
2025-06-18 10:31:01 -0500

    Refactor lógica de validación de formulario de inicio de sesión y mejora la gestión de errores

--- peluqueria/views/login/login_form/login_form.py
+++ peluqueria/views/login/login_form/login_form.py
@@ -13,37 +13,29 @@ class LoginState(rx.State):
     form_data: dict = {}
 
     initial_email_error_state: str = "none"
-    email_value: str = ""
 
     initial_password_error_state: str = "none"
-    password_value: str = ""
-
-    @rx.event
-    def email_validation(self) -> None:
-        if len(self.email_value) == 0:
-            self.initial_email_error_state = "block"
-        elif re.match(EMAIL_REGEX, self.email_value):
-            self.initial_email_error_state = "none"
-        else:
-            self.initial_email_error_state = "block"
-
-    @rx.event
-    def password_validation(self, value: str) -> None:
-        self.password_value = value
-        if len(self.password_value) >= 8:
-            self.initial_password_error_state = "none"
-        else:
-            self.initial_password_error_state = "block"
 
     @rx.event
     async def handle_submit(self, form_data: dict):
-        self.email_value = ""
-        self.password_value = ""
+        valid_email = False
+        valid_password = False
 
-        if (
-            re.match(EMAIL_REGEX, form_data.get("email") or "")
-            and len(form_data.get("password") or "") >= 8
-        ):
+        if re.match(EMAIL_REGEX, form_data.get("email") or ""):
+            valid_email = True
+            self.initial_email_error_state = "none"
+        else:
+            valid_email = False
+            self.initial_email_error_state = "block"
+
+        if len(form_data.get("password") or "") >= 8:
+            valid_password = True
+            self.initial_password_error_state = "none"
+        else:
+            valid_password = False
+            self.initial_password_error_state = "block"
+
+        if valid_email and valid_password:
             auth_state = await self.get_state(AuthState)
             async for action in auth_state.login(
                 form_data["email"], form_data["password"]
@@ -70,9 +62,6 @@ def login_form() -> rx.Component:
                         "email",
                         "email",
                         LoginState.initial_email_error_state,
-                        LoginState.email_value,
-                        LoginState.set_email_value,  # type: ignore
-                        LoginState.email_validation,
                         "Por favor, ingresa un correo electrónico valido.",
                     ),
                     margin_bottom="0.5rem",
@@ -84,13 +73,21 @@ def login_form() -> rx.Component:
                         "password",
                         "password",
                         LoginState.initial_password_error_state,
-                        LoginState.password_value,
-                        LoginState.password_validation,
                         "Por favor ingrese una contraseña de mínimo 8 caracteres.",
                     ),
                     margin_bottom="1rem",
                 ),
                 rx.separator(margin_bottom="1rem"),
+                rx.text(
+                    "¿No tienes una cuenta? ",
+                    rx.link(
+                        "Regístrate desde aquí",
+                        href="/registro",
+                        color=Colors.PRIMARY_COLOR.value,
+                    ),
+                    margin_bottom="1rem",
+                    size="2",
+                ),
                 rx.button(
                     rx.cond(AuthState.loading, rx.spinner(size="2"), "Iniciar Sesión"),
                     type="submit",
@@ -103,6 +100,6 @@ def login_form() -> rx.Component:
             width=rx.breakpoints(initial="80%", md="40%"),
         ),
         margin_y="2rem",
-        height="60vh",
+        min_height="60vh",
         width="100%",
     )
