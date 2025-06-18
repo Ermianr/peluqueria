0b18a43a82269c2d24f284b80edb28a4886dcd56
Kevin Garcia <ermianrazor@gmail.com>
2025-06-18 10:27:22 -0500

    Agregar gestión del ciclo de vida de la aplicación y crear índices al iniciar la API.

--- peluqueria/api/main.py
+++ peluqueria/api/main.py
@@ -1,8 +1,19 @@
+from contextlib import asynccontextmanager
+
 from fastapi import FastAPI
 
-from peluqueria.api.routers import auth, users
+from peluqueria.api.db.db_connect import create_indexes
+from peluqueria.api.routers import auth, services, users
 
-fastapi_app: FastAPI = FastAPI()
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    await create_indexes()
+    yield
+
+
+fastapi_app: FastAPI = FastAPI(lifespan=lifespan)
 
 fastapi_app.include_router(users.router)
 fastapi_app.include_router(auth.router)
+fastapi_app.include_router(services.router)
