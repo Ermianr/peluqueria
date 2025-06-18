a87d70753de950f88be57fbf46e8643c19409961
Kevin Garcia <ermianrazor@gmail.com>
2025-06-18 10:29:32 -0500

    Refactor la función navbar_desktop y navbar_mobile: mejora la estructura del código y actualiza los enlaces de navegación.

--- peluqueria/components/navbar.py
+++ peluqueria/components/navbar.py
@@ -6,97 +6,130 @@ from peluqueria.styles.styles import OUTLINE_BUTTON, SIMPLE_BUTTON, Colors
 
 
 def navbar_desktop():
-    return (
-        rx.desktop_only(
+    return rx.desktop_only(
+        rx.hstack(
             rx.hstack(
-                rx.hstack(
+                rx.link(
                     rx.heading(
                         "divine",
                         size="7",
                         weight="bold",
                         color=Colors.CUSTOM_WHITE.value,
                     ),
-                    rx.spacer(),
-                    rx.spacer(),
-                    rx.hstack(
-                        navbar_link("Inicio", "/#"),
-                        navbar_link("Servicios", "/#"),
-                        navbar_link("Nosotros", "/#"),
-                        navbar_link("Contacto", "/#"),
-                        spacing="5",
-                    ),
-                    align_items="center",
+                    href="/",
                 ),
-                rx.cond(
-                    AuthState.is_authenticated,
-                    rx.hstack(
-                        rx.text(
-                            "Hola, ",
-                            AuthState.user_data.get("first_name", "Usuario"),
-                            color=Colors.CUSTOM_WHITE,
-                        ),
-                        rx.el.button(
-                            "Cerrar Sesión",
-                            style=OUTLINE_BUTTON,
-                            on_click=AuthState.logout,
-                        ),
-                        spacing="4",
-                        justify="end",
-                    ),
-                    rx.hstack(
-                        rx.link(
-                            rx.el.button("Registrarse", style=SIMPLE_BUTTON),
-                            href="/registro",
-                        ),
-                        rx.link(
-                            rx.el.button("Iniciar Sesión", style=OUTLINE_BUTTON),
-                            href="/ingreso",
-                        ),
-                        spacing="4",
-                        justify="end",
-                    ),
+                rx.spacer(),
+                rx.spacer(),
+                rx.hstack(
+                    navbar_link("Inicio", "/"),
+                    navbar_link("Servicios", "#services"),
+                    navbar_link("Nosotros", "#about"),
+                    navbar_link("Contacto", "#contact"),
+                    spacing="5",
                 ),
-                justify="between",
                 align_items="center",
             ),
+            rx.cond(
+                AuthState.is_authenticated,
+                rx.hstack(
+                    rx.text(
+                        "Hola, ",
+                        AuthState.user_data.get("first_name", "Usuario"),
+                        color=Colors.CUSTOM_WHITE.value,
+                    ),
+                    rx.el.button(
+                        "Cerrar Sesión",
+                        style=OUTLINE_BUTTON,
+                        on_click=AuthState.logout,
+                    ),
+                    spacing="4",
+                    justify="end",
+                    align_items="center",
+                ),
+                rx.hstack(
+                    rx.link(
+                        rx.el.button("Registrarse", style=SIMPLE_BUTTON),
+                        href="/registro",
+                    ),
+                    rx.link(
+                        rx.el.button("Iniciar Sesión", style=OUTLINE_BUTTON),
+                        href="/ingreso",
+                    ),
+                    spacing="4",
+                    justify="end",
+                ),
+            ),
+            justify="between",
+            align_items="center",
         ),
     )
 
 
 def navbar_mobile():
-    return (
-        rx.mobile_and_tablet(
+    return rx.mobile_and_tablet(
+        rx.hstack(
             rx.hstack(
-                rx.hstack(
+                rx.link(
                     rx.heading(
                         "divine",
                         size="6",
                         weight="bold",
                         color=Colors.CUSTOM_WHITE.value,
                     ),
-                    align_items="center",
+                    href="/",
                 ),
-                rx.menu.root(
-                    rx.menu.trigger(
-                        rx.icon("menu", size=30, color=Colors.CUSTOM_WHITE.value)
-                    ),
-                    rx.menu.content(
-                        rx.menu.item("Inicio"),
-                        rx.menu.item("Servicios"),
-                        rx.menu.item("Nosotros"),
-                        rx.menu.item("Contacto"),
-                        rx.menu.separator(),
-                        rx.menu.item(rx.link("Registrarse", href="/registro")),
-                        rx.menu.item(rx.link("Iniciar Sesión", href="/ingreso")),
-                        color_scheme="amber",
-                        background="black",
-                        color=Colors.CUSTOM_WHITE.value,
-                    ),
-                    justify="end",
-                ),
-                justify="between",
                 align_items="center",
             ),
+            rx.menu.root(
+                rx.menu.trigger(
+                    rx.icon("menu", size=30, color=Colors.CUSTOM_WHITE.value)
+                ),
+                rx.menu.content(
+                    rx.menu.item("Inicio"),
+                    rx.menu.item("Servicios"),
+                    rx.menu.item("Nosotros"),
+                    rx.menu.item("Contacto"),
+                    rx.menu.separator(),
+                    rx.cond(
+                        AuthState.is_authenticated,
+                        rx.menu.item(
+                            rx.text(
+                                f"Hola, {AuthState.user_data.get('first_name', 'Usuario')}",
+                                color=Colors.CUSTOM_WHITE.value,
+                            )
+                        ),
+                        None,
+                    ),
+                    rx.cond(
+                        AuthState.is_authenticated,
+                        rx.menu.item("Cerrar Sesión", on_click=AuthState.logout),
+                        [
+                            rx.menu.item(
+                                rx.link(
+                                    "Registrarse",
+                                    href="/registro",
+                                    text_decoration="none",
+                                ),
+                                key="registrarse",
+                            ),
+                            rx.menu.item(
+                                rx.link(
+                                    "Iniciar Sesión",
+                                    href="/ingreso",
+                                    text_decoration="none",
+                                ),
+                                key="ingreso",
+                            ),
+                        ],
+                    ),
+                    color_scheme="amber",
+                    background="black",
+                    color=Colors.CUSTOM_WHITE.value,
+                ),
+                justify="end",
+            ),
+            justify="between",
+            align_items="center",
         ),
     )
 
