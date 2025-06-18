ac03df0e118d5955c6e3c51a0e544a7a0af2e118
Kevin Garcia <ermianrazor@gmail.com>
2025-06-18 10:29:28 -0500

    Refactor la función navbar_link: reemplaza href y text_decoration por un evento on_click para mejorar la navegación.

--- peluqueria/components/navbar_link.py
+++ peluqueria/components/navbar_link.py
@@ -3,9 +3,14 @@ import reflex as rx
 from peluqueria.styles.styles import NAVBAR_LINK
 
 
+class NavbarLinkState(rx.State):
+    @rx.event
+    async def go_to_section(self, section: str):
+        return rx.redirect(section)
+
+
 def navbar_link(text: str, url: str) -> rx.Component:
     return rx.link(
         rx.text(text, size="4", weight="medium", style=NAVBAR_LINK),
-        href=url,
-        text_decoration="none",
+        on_click=NavbarLinkState.go_to_section(url),
     )
