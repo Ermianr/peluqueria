dafc3cccc40cbb455ec26c1e136d48f61817da52
Kevin Garcia <ermianrazor@gmail.com>
2025-06-25 20:28:34 -0500

    Eliminar funciones de verificación y búsqueda de usuarios en utils.py para simplificar el código.

--- peluqueria/api/utils/utils.py
+++ peluqueria/api/utils/utils.py
@@ -1,9 +1,4 @@
 import bcrypt
-from fastapi import HTTPException, status
-
-from peluqueria.api.db.db_connect import db
-from peluqueria.api.models.user import UserInDBResponse, UserResponse
-from peluqueria.api.schemas.user import user_db_response_schema, user_response_schema
 
 
 def hash_password(password: str) -> str:
@@ -17,28 +12,13 @@ def verify_password(plain_password: str, hashed_password: str) -> bool:
     )
 
 
-async def check_if_user_exist(email: str) -> bool:
-    user = await db.users.find_one({"email": email})
-    return user is not None
+def id_to_pydantic(doc: dict) -> None:
+    doc["id"] = str(doc["_id"])
+    del doc["_id"]
 
 
-async def search_user(field: str, key) -> UserResponse:
-    user = await db.users.find_one({field: key})
-
-    if not user:
-        raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND, detail="User not found."
-        )
-
-    return UserResponse(**user_response_schema(user))
-
-
-async def search_user_db(field: str, key) -> UserInDBResponse:
-    user = await db.users.find_one({field: key})
-
-    if not user:
-        raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND, detail="User not found"
-        )
-
-    return UserInDBResponse(**user_db_response_schema(user))
+### Helper para un bucle y transformar un id a formato pydantic
+def id_to_pydantic_loop(doc: dict) -> dict:
+    if "_id" in doc:
+        id_to_pydantic(doc)
+    return doc
