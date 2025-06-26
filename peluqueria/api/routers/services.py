33979ac6a6b69e432905a8a7a52eec84afdc6c5a
Kevin Garcia <ermianrazor@gmail.com>
2025-06-25 20:27:58 -0500

    Refactor el manejo de servicios en services.py para simplificar la transformación de IDs y mejorar la lógica de actualización de servicios.

--- peluqueria/api/routers/services.py
+++ peluqueria/api/routers/services.py
@@ -11,6 +11,11 @@ from peluqueria.api.models.service import (
     ServiceResponse,
     ServiceUpdateAdmin,
 )
+from peluqueria.api.utils.service_utils import (
+    check_if_service_exist,
+    get_service_or_404,
+)
+from peluqueria.api.utils.utils import id_to_pydantic, id_to_pydantic_loop
 
 router: APIRouter = APIRouter(prefix="/services", tags=["services"])
 
@@ -28,47 +33,24 @@ async def create_service(service: Service):
 
     service_dict = service_in_db.model_dump()
 
-    id = (await db.services.insert_one(service_dict)).inserted_id
-    service_dict["id"] = str(id)
+    service_id = (await db.services.insert_one(service_dict)).inserted_id
+    service_dict["id"] = str(service_id)
     return ServiceResponse.model_validate(service_dict)
-
-
-### Helper para transformar los id a formato pydantic
-def transform_id(doc: dict) -> dict:
-    if "_id" in doc:
-        doc["id"] = str(doc["_id"])
-        del doc["_id"]
-    return doc
 
 
 @router.get("", response_model=list[ServiceResponse])
 async def get_all_services():
     services = await db.services.find().to_list(length=None)
     return [
-        ServiceResponse.model_validate(transform_id(service)) for service in services
+        ServiceResponse.model_validate(id_to_pydantic_loop(service))
+        for service in services
     ]
 
 
-### Helper para buscar
-async def check_if_service_exist(name: str) -> bool:
-    service = await db.services.find_one({"name": name})
-    return service is not None
-
-
-async def get_service_or_404(id: str) -> ServiceResponse:
-    service = await db.services.find_one({"_id": ObjectId(id)})
-    if not service:
-        raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND, detail="Service not found."
-        )
-    service["id"] = str(service["_id"])
-    del service["_id"]
-    return ServiceResponse.model_validate(service)
-
-
-@router.patch("/{id}", response_model=ServiceResponse)
-async def update_user(
+@router.patch("/{service_id}", response_model=ServiceResponse)
+async def update_service(
     service_update_data: ServiceUpdateAdmin,
+    service_id: str,
     service: Annotated[ServiceResponse, Depends(get_service_or_404)],
 ):
     update_data = service_update_data.model_dump(exclude_unset=True)
@@ -79,12 +61,15 @@ async def update_user(
             detail="No update data provided.",
         )
 
-    if "name" in update_data and update_data["name"] != service.name:
-        if await check_if_service_exist(update_data["name"]):
-            raise HTTPException(
-                status_code=status.HTTP_409_CONFLICT,
-                detail="Service already registered by another service.",
-            )
+    if (
+        "name" in update_data
+        and update_data["name"] != service.name
+        and await check_if_service_exist(update_data["name"])
+    ):
+        raise HTTPException(
+            status_code=status.HTTP_409_CONFLICT,
+            detail="Service already registered by another service.",
+        )
 
     update_data["updated_at"] = datetime.now(timezone.utc)
 
@@ -100,7 +85,6 @@ async def update_user(
             detail="Service not found after update.",
         )
 
-    updated_service_doc["id"] = str(updated_service_doc["_id"])
-    del updated_service_doc["_id"]
+    id_to_pydantic(updated_service_doc)
 
     return ServiceResponse.model_validate(updated_service_doc)
