b179d8a4dc4e5109210f64ceda04d7323f896926
Kevin Garcia <ermianrazor@gmail.com>
2025-06-25 20:27:41 -0500

    Agregar variables de configuración para la clave secreta de hash de contraseña y la expiración del token de acceso en settings.py

--- peluqueria/settings.py
+++ peluqueria/settings.py
@@ -9,3 +9,5 @@ class Settings:
     MONGO_DB_URI: str = os.getenv("MONGO_DB_URI") or "mongodb://localhost:27017/"
     MONGO_DB_NAME: str = os.getenv("MONGO_DB_NAME") or "local"
     API_BACKEND_URL: str = os.getenv("API_BACKEND_URL") or "http://localhost:8000"
+    HASH_PASSWORD_SECRET_KEY: str = os.getenv("HASH_PASSWORD_SECRET_KEY=") or "default"
+    ACCESS_TOKEN_EXPIRE_DAYS: int = int(os.getenv("ACCESS_TOKEN_EXPIRE_DAYS") or 3)
